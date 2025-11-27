fe036325a2c362f9ab5ca7043ceb41b7198778d7
Mikolaj Pasiut <mikpas1@st.amu.edu.pl>
2025-11-27 23:56:45 +0100

    Done some stuff

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,7 @@
-import Hello from "../components/hello";
-
 const Home = () => {
-  console.log("server component rendered");
   return (
     <>
-      <Hello />
-      <div className="text-5xl underline">Welcome to next.js</div>
+      <h1 className="">Welcome to next.js</h1>
     </>
   );
 };
--- components/hello.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-const Hello = () => {
-  console.log("client component rendered");
-  return <div>Hello</div>;
-};
-
-export default Hello;
