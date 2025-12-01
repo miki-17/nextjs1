d1d3992ee59a0067eb8921ed490c91988eba041f
Mikolaj Pasiut <mikpas1@st.amu.edu.pl>
2025-12-01 17:43:58 +0100

    Small change

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ const Home = () => {
     <section>
       <h1 className="text-center">
         The Hub For Every Dev
-        <br /> Event You Can&apos;t Miss!
+        <br /> Event You Can&apos;t Miss!!
       </h1>
       <p className="text-center mt-5">
         Hackathons, Meetups and Conferences, All in One Place
